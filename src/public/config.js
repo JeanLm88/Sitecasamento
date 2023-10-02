0fe8d872f0a9f2514f003bd30abf2d024636dc0c
LIMA Jean <jlima15@elisbrasil.net>
2023-10-02 09:33:39 -0300

    fiz alteração no botão de adicionar nome para desativar a submissão, e alterei o host

--- src/public/config.js
+++ src/public/config.js
@@ -2,7 +2,7 @@ import {config} from "dotenv";
 config();
 
 export const PORT = 3000;
-export const HOST = `http://localhost:${PORT}`;
+export const HOST = `https://listadepresentesbj.netlify.app:${PORT}`;
 
 export const MERCADOPAGO_API_KEY = process.env.MERCADOPAGO_API_KEY;
 
